02e2392283d44e272d1f9ca3a6d4328eb20ea746
cmlenius <cameron.lenius@gmail.com>
2019-02-15 12:28:38 -0500

    cleanup main

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-let canvas, gl;
-
-
 function main() {
-    canvas = document.querySelector("#glCanvas");
-    gl = canvas.getContext("webgl2");
+    let canvas = document.querySelector("#glCanvas");
+    let gl = canvas.getContext("webgl2");
 
     if (gl == null) {
         alert("Unable to initialize WebGl2");
         return;
     }
-
-    // window.addEventListener('resize', onResizeWindow);
-    // onResizeWindow();
-
     const shaderProgram = glUtils.initShaderProgram(gl);
     gl.useProgram(shaderProgram);
 
-    var uniforms = {
-        viewportDimesions: gl.getUniformLocation(shaderProgram, 'viewportDimensions'),
-        minI: gl.getUniformLocation(shaderProgram, 'g_seed'),
-    };
-
-    var vpDimesions = [canvas.width, canvas.height];
-
-    var vertexBuffer = gl.createBuffer();
     var verticies = [
         -1, 1,
         -1, -1,
@@ -33,7 +18,7 @@ function main() {
         1, 1,
         1, -1
     ];
-
+    var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticies), gl.STATIC_DRAW);
 
@@ -45,26 +30,13 @@ function main() {
     function draw() {
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
-
-        gl.uniform2fv(uniforms.viewportDimesions, vpDimesions);
-
         gl.drawArrays(gl.TRIANGLES, 0, 6);
 
-
-        // requestAnimationFrame(draw);
+        requestAnimationFrame(draw);
     }
 
     draw();
-    // requestAnimationFrame(draw);
-}
-
-function onResizeWindow() {
-    if (!canvas) return;
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    gl.viewport(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(draw);
 }
 
 main();
