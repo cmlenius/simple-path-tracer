f657ebdd082b035a24c43760771bd1f2dca47f09
Cameron Mark Lenius <cmlenius@uwaterloo.ca>
2019-03-09 14:33:56 -0500

    move shader to different file

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 let canvas = document.querySelector("#glCanvas");
-const verticies = [
-    -1, 1,
-    -1, -1,
-    1, -1,
-
-    -1, 1,
-    1, 1,
-    1, -1
-];
-
-let MAX_SAMPLES = 30.0;
-let mouseDown = false;
-let sampleCount = 1.0
-let zoomZ = 2.5;
-let angleX = 0.0;
-let angleY = 0.0;
+ 
+const LAMBERTIAN = 0,
+      METAL = 1,
+      DIELECTRIC = 2;
+const MAX_SAMPLES = 30.0;
+let mouseDown = false,
+    sampleCount = 0.0,
+    angleX = 0.0,
+    angleY = 0.0;
+    zoomZ = 2.5;
 let px, py;
+
+/*
+function make_objects() {
+    var objects = [];
+    objects.push([[0.0, -100.5, -1.0], 100.0, METAL] 
+        objects[0] = sphere(vec3(0.0, -100.5, -1.0), 100.0, material(METAL, vec3(0.2, 0.2, 0.2), 0.05));
+        objects[1] = sphere(vec3(1.0, 0.0, 0.0), 0.5, material(METAL, vec3(0.7, 0.2, 0.2), 0.0));
+        objects[2] = sphere(vec3(0.0, 1.0, 0.0), 0.5, material(METAL, vec3(0.2, 0.7, 0.2), 0.0));
+        objects[3] = sphere(vec3(-1.0, 0.0, 0.0), 0.5, material(METAL, vec3(0.2, 0.2, 0.7), 0.0));
+     
+}
+*/
 
 function main() {
     let gl = canvas.getContext("webgl2");
@@ -61,10 +67,8 @@ function main() {
             gl.clear(gl.COLOR_BUFFER_BIT);
             gl.drawArrays(gl.TRIANGLES, 0, 6);
         }
-         
         requestAnimationFrame(draw); 
     }
-   
     requestAnimationFrame(draw); 
 }
 
